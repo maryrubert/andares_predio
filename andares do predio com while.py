99e3a67ec724b948575e0f5c2f6e9059e49e87d2
MARIANA RUBERT GRECCO <55103645+maryrubert@users.noreply.github.com>
2023-07-30 17:30:26 -0300

    Update andares do predio com while.py

--- andares do predio com while.py
+++ andares do predio com while.py
@@ -1,8 +1,8 @@
 numero = 21
-while (numero<21):
+while (numero<=21):
     numero=numero-1
     if(numero==13):
         continue
-    if(numero==0):
+    if(numero==-1):
       break
     print(numero)
