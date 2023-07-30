bc9c5ce7147cc43919fbc3d8d36d5677de9ef9ad
MARIANA RUBERT GRECCO <55103645+maryrubert@users.noreply.github.com>
2023-07-30 17:12:47 -0300

    Add files via upload

---
+++ andares do predio com for.py
@@ -0,0 +1,7 @@
+numero = 21
+for i in range(21):
+  numero = numero - 1
+  if (numero == 13):
+    continue
+  print (numero)
+    
---
+++ andares do predio com while.py
@@ -0,0 +1,8 @@
+numero = 21
+while (numero<21):
+    numero=numero-1
+    if(numero==13):
+        continue
+    if(numero==0):
+      break
+    print(numero)
